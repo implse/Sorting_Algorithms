2c3cfe2368192a40193f93801a33f810ea67dce9
Finite <finite.impulse@gmail.com>
2019-04-19 06:11:36 +0200

    Selection Sort Time Complexity

--- SelectionSort/SelectionSort.py
+++ SelectionSort/SelectionSort.py
@@ -1,9 +1,11 @@
+# Selection Sort : Time Complexity O(n^2)  / Space Complexity O(1) Space
+
 def selection_sort(data):
     for i in range(len(data) - 1):
-        index = i
+        smallestIdx = i
         for j in range(i + 1, len(data), 1):
-            if data[j] < data[index]:
-                index = j
-        if index != i:
-            data[index], data[i] = data[i], data[index]
+            if data[j] < data[smallestIdx]:
+                smallestIdx = j
+        if smallestIdx != i:
+            data[smallestIdx], data[i] = data[i], data[smallestIdx]
     return data
