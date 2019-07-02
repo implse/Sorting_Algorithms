6e0d383e0eb437bb10ecc2dc4075ab0771763d5f
Finite <finite.impulse@gmail.com>
2019-07-02 21:32:46 +0200

    Update Quicksort

--- BubbleSort/BubbleSort.py
+++ BubbleSort/BubbleSort.py
@@ -1,5 +1,4 @@
 # Bubble Sort : Time Complexity O(n^2)
-
 def bubble_sort(data):
     for i in range(len(data) - 1):
         for j in range(len(data) - 1 - i):
--- QuickSort/QuickSort.py
+++ QuickSort/QuickSort.py
@@ -1,8 +1,11 @@
 # Quick Sort : Time Complexity O(n log(n)) / Space Complexity O(log(n))
+
+# Main Function
 def quickSort(array):
 	quicksortHelper(array, 0, len(array) - 1)
 	return array
 
+# Recursive Function
 def quicksortHelper(array, low, high):
 	# Base case
 	if low >= high:
@@ -14,16 +17,19 @@ def quicksortHelper(array, low, high):
 	# Recursive call right
 	quicksortHelper(array, pivotIdx + 1, high)
 
+# Partition Function : partition the array around the pivot
 def partition(array, low, high):
+	# Choose a pivot in the middle index
 	pivotIdx = (low + high) // 2
 	swap(array, pivotIdx, high)
 	i = low
-	for j in range(low, high, 1):
+	for j in range(low, high):
 		if array[j] <= array[high]:
 			swap(array, i, j)
 			i += 1
 	swap(array, i, high)
 	return i
 
+# Swap Function
 def swap(array, i, j):
 	array[i], array[j] = array[j], array[i]
