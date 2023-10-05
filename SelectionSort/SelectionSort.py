a3b94433080f94334fe0d01ffbb0f883b535d72e
Finite <finite.impulse@gmail.com>
2023-10-05 07:08:44 +0200

    update selection sort code

--- SelectionSort/SelectionSort.py
+++ SelectionSort/SelectionSort.py
@@ -1,10 +1,10 @@
 # Selection Sort : Time Complexity O(n^2)  / Space Complexity O(1) Space
 def selection_sort(data):
-    for i in range(len(data) - 1):
-        smallestIdx = i
+    for i in range(len(data)):
+        min_idx = i
         for j in range(i + 1, len(data)):
-            if data[j] < data[smallestIdx]:
-                smallestIdx = j
-        if smallestIdx != i:
-            data[smallestIdx], data[i] = data[i], data[smallestIdx]
-    return data
+            if data[min_idx] > data[j]:
+                min_idx = j
+        if min_idx != i:
+            data[min_idx], data[i] = data[i], data[min_idx]
+    return
