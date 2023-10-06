eb7fffb717462a22f52910f661dd1481567f3e34
Finite <finite.impulse@gmail.com>
2023-10-06 06:56:23 +0200

    update merge sort

--- MergeSort/MergeSort.py
+++ MergeSort/MergeSort.py
@@ -1,20 +1,21 @@
 # MergeSort : Time Complexity O(n log(n)) / Space Complexity O(1)
+
+# Divide : Split and recursively call the two parts
 def mergeSort(lst):
     if len(lst) == 1:
         return lst
-    # Divide
-    middleIdx = len(lst) // 2
+    middleIdx = len(lst)//2
     left = lst[:middleIdx]
     right = lst[middleIdx:]
     # Recursive calls on each half
     left_sorted = mergeSort(left)
     right_sorted = mergeSort(right)
     # Merge
-    merge_result = mergeSortlist(left_sorted, right_sorted, lst)
-    return merge_result
+    merge = mergeSortlist(left_sorted, right_sorted, lst)
+    return merge
 
 
-# Helper : Merge two sorted list with no extra space : Space O(1)
+# Conquer : Merge two sorted list with no extra space : Space O(1)
 def mergeSortlist(left, right, lst):
     # Iterator for the main list
     k = 0
@@ -40,50 +41,4 @@ def mergeSortlist(left, right, lst):
         j += 1
         k += 1
     return lst
-
-
-# MergeSort : Time Complexity O(n log(n)) / Space Complexity O(n)
-def mergeSort(lst):
-    if len(lst) == 1:
-        return lst
-    # Divide
-    middleIdx = len(lst) // 2
-    left = lst[:middleIdx]
-    right = lst[middleIdx:]
-    # Recursive calls on each half
-    left_sorted = mergeSort(left)
-    right_sorted = mergeSort(right)
-    # Merge
-    merge_result = mergeSortlist(left_sorted, right_sorted)
-    return merge_result
-
-# Helper : Merge two sorted list with extra space for the sorted list : Space O(n)
-def mergeSortlist(left, right):
-    ln_left = len(left)
-    ln_right = len(right)
-    # Sorted list
-    sortedlist = [None for _ in range(ln_left + ln_right)]
-    # Iterators
-    k = 0
-    i = 0
-    j = 0
-    # Merge
-    while i < ln_left and j < ln_right:
-        if left[i] <= right[j]:
-            sortedlist[k] = left[i]
-            i += 1
-        else:
-            sortedlist[k] = right[j]
-            j += 1
-        k += 1
-    # All remaining values
-    while i < ln_left:
-        sortedlist[k] = left[i]
-        i += 1
-        k += 1
-    while j < ln_right:
-        sortedlist[k] = right[j]
-        j += 1
-        k += 1
-    return sortedlist
 
